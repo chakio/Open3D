9a1f7d26fc996f157f0ae655e8dbf4567bbb9032
Yixing Lao <yixing.lao@gmail.com>
2020-08-28 11:40:15 -0700

    partial work

--- cpp/pybind/ml/contrib.cpp
+++ cpp/pybind/ml/contrib.cpp
@@ -27,17 +27,35 @@
 #include "open3d/ml/contrib/GridSubsampling.h"
 #include "pybind/docstring.h"
 #include "pybind/open3d_pybind.h"
+#include "pybind/pybind_utils.h"
 
 namespace open3d {
 namespace ml {
 
-void pybind_contrib(py::module &m) {
+// Converts py::array to Tensor. There will not be a memory copy. Tensor allows
+// easy manipulations such as checking dtype and converting to contiguous
+// array.
+core::Tensor PyArrayToTensor(py::array array) {
+    py::buffer_info info = array.request();
+    core::SizeVector shape(info.shape.begin(), info.shape.end());
+    core::SizeVector strides(info.strides.begin(), info.strides.end());
+    for (size_t i = 0; i < strides.size(); ++i) {
+        strides[i] /= info.itemsize;
+    }
+    core::Dtype dtype = pybind_utils::ArrayFormatToDtype(info.format);
+    core::Device device("CPU:0");
+    std::function<void(void*)> deleter = [](void*) -> void {};
+    auto blob = std::make_shared<core::Blob>(device, info.ptr, deleter);
+    return core::Tensor(shape, strides, info.ptr, dtype, blob);
+}
+
+void pybind_contrib(py::module& m) {
     py::module m_contrib = m.def_submodule("contrib");
 
     m_contrib.def(
             "subsample",
-            [](py::array_t<float> points, py::array_t<float> features,
-               py::array_t<int> classes, float sampleDl, int verbose) {
+            [](py::array points, py::array features, py::array classes,
+               float sampleDl, int verbose) {
                 std::vector<contrib::PointXYZ> original_points;
                 std::vector<contrib::PointXYZ> subsampled_points;
                 std::vector<float> original_features;
@@ -45,15 +63,22 @@ void pybind_contrib(py::module &m) {
                 std::vector<int> original_classes;
                 std::vector<int> subsampled_classes;
 
-                grid_subsampling(original_points, subsampled_points,
-                                 original_features, subsampled_features,
-                                 original_classes, subsampled_classes, sampleDl,
-                                 verbose);
+                core::Tensor points_t = PyArrayToTensor(points);
+
+                if (classes != py::none()) {
+                    core::Tensor classes_t = PyArrayToTensor(classes);
+                }
+
+                // grid_subsampling(original_points, subsampled_points,
+                //                  original_features, subsampled_features,
+                //                  original_classes, subsampled_classes,
+                //                  sampleDl, verbose);
 
                 return py::make_tuple(subsampled_points, subsampled_features,
                                       subsampled_classes);
             },
-            "points"_a, "features"_a, "classes"_a, "sampleDl"_a, "verbose"_a);
+            "points"_a, "features"_a = py::none(), "classes"_a = py::none(),
+            "sampleDl"_a = 0.1f, "verbose"_a = 0);
 }
 
 }  // namespace ml

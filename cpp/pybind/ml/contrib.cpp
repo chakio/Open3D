3d1561388f6c3b80c6edfe7d3c71f03a8a244bfd
Yixing Lao <yixing.lao@gmail.com>
2020-08-28 23:41:36 -0700

    TensorToPyArray and PyArrayToTensor

--- cpp/pybind/ml/contrib.cpp
+++ cpp/pybind/ml/contrib.cpp
@@ -25,93 +25,13 @@
 // ----------------------------------------------------------------------------
 
 #include "open3d/ml/contrib/GridSubsampling.h"
+#include "pybind/core/core.h"
 #include "pybind/docstring.h"
 #include "pybind/open3d_pybind.h"
 #include "pybind/pybind_utils.h"
 
 namespace open3d {
 namespace ml {
-
-// Converts a py::array to a contiguous Tensor.
-//
-// If the input is contiguous, there won't be a copy, otherwise, the tensor will
-// be copied to convert to contiguous. Tensor allows easy manipulations such as
-// checking dtype and converting to contiguous array.
-core::Tensor PyArrayToContiguousTensor(py::array array) {
-    py::buffer_info info = array.request();
-    core::SizeVector shape(info.shape.begin(), info.shape.end());
-    core::SizeVector strides(info.strides.begin(), info.strides.end());
-    for (size_t i = 0; i < strides.size(); ++i) {
-        strides[i] /= info.itemsize;
-    }
-    core::Dtype dtype = pybind_utils::ArrayFormatToDtype(info.format);
-    core::Device device("CPU:0");
-    std::function<void(void*)> deleter = [](void*) -> void {};
-    auto blob = std::make_shared<core::Blob>(device, info.ptr, deleter);
-    return core::Tensor(shape, strides, info.ptr, dtype, blob).Contiguous();
-}
-
-py::array TensorToPyArray(const core::Tensor& tensor) {
-    if (tensor.GetDevice().GetType() != core::Device::DeviceType::CPU) {
-        utility::LogError(
-                "Can only convert CPU Tensor to numpy. Copy "
-                "Tensor to CPU before converting to numpy.");
-    }
-    py::dtype py_dtype =
-            py::dtype(pybind_utils::DtypeToArrayFormat(tensor.GetDtype()));
-    py::array::ShapeContainer py_shape(tensor.GetShape());
-    core::SizeVector strides = tensor.GetStrides();
-    int64_t element_byte_size = tensor.GetDtype().ByteSize();
-    for (auto& s : strides) {
-        s *= element_byte_size;
-    }
-    py::array::StridesContainer py_strides(strides);
-
-    // `base_tensor` is a shallow copy of `tensor`. `base_tensor`
-    // is on the heap and is owned by py::capsule
-    // `base_tensor_capsule`. The capsule is referenced as the
-    // "base" of the numpy tensor returned by o3d.Tensor.numpy().
-    // When the "base" goes out-of-scope (e.g. when all numpy
-    // tensors referencing the base have gone out-of-scope), the
-    // deleter is called to free the `base_tensor`.
-    //
-    // This behavior is important when the origianl `tensor` goes
-    // out-of-scope while we still want to keep the data alive.
-    // e.g.
-    //
-    // ```python
-    // def get_np_tensor():
-    //     o3d_t = o3d.Tensor(...)
-    //     return o3d_t.numpy()
-    //
-    // # Now, `o3d_t` is out-of-scope, but `np_t` still
-    // # references the base tensor which references the
-    // # underlying data of `o3d_t`. Thus np_t is still valid.
-    // # When np_t goes out-of-scope, the underlying data will be
-    // # finally freed.
-    // np_t = get_np_tensor()
-    // ```
-    //
-    // See:
-    // https://stackoverflow.com/questions/44659924/returning-numpy-arrays-via-pybind11
-    core::Tensor* base_tensor = new core::Tensor(tensor);
-
-    // See PyTorch's torch/csrc/Module.cpp
-    auto capsule_destructor = [](PyObject* data) {
-        core::Tensor* base_tensor = reinterpret_cast<core::Tensor*>(
-                PyCapsule_GetPointer(data, "open3d::core::Tensor"));
-        if (base_tensor) {
-            delete base_tensor;
-        } else {
-            PyErr_Clear();
-        }
-    };
-
-    py::capsule base_tensor_capsule(base_tensor, "open3d::core::Tensor",
-                                    capsule_destructor);
-    return py::array(py_dtype, py_shape, py_strides, tensor.GetDataPtr(),
-                     base_tensor_capsule);
-}
 
 void pybind_contrib(py::module& m) {
     py::module m_contrib = m.def_submodule("contrib");
@@ -128,7 +48,8 @@ void pybind_contrib(py::module& m) {
                 std::vector<int> subsampled_classes;
 
                 // Fill original_points.
-                core::Tensor points_t = PyArrayToContiguousTensor(points);
+                core::Tensor points_t =
+                        core::PyArrayToTensor(points, true).Contiguous();
                 if (points_t.GetDtype() != core::Dtype::Float32) {
                     utility::LogError("points must be np.float32.");
                 }
@@ -148,7 +69,7 @@ void pybind_contrib(py::module& m) {
                 // Fill original_features.
                 if (features != py::none()) {
                     core::Tensor features_t =
-                            PyArrayToContiguousTensor(features);
+                            core::PyArrayToTensor(features, true).Contiguous();
                     if (features_t.GetDtype() != core::Dtype::Float32) {
                         utility::LogError("features must be np.float32.");
                     }
@@ -170,7 +91,8 @@ void pybind_contrib(py::module& m) {
 
                 // Fill original_classes.
                 if (classes != py::none()) {
-                    core::Tensor classes_t = PyArrayToContiguousTensor(classes);
+                    core::Tensor classes_t =
+                            core::PyArrayToTensor(classes, true).Contiguous();
                     if (classes_t.GetDtype() != core::Dtype::Int32) {
                         utility::LogError("classes must be np.int32.");
                     }
